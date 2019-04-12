b58b54b0f896a35f0cd4812244f84b8fd4e176d6
pedroperrusi <suruagyperrusi@etu.unistra.fr>
2019-04-12 22:16:48 +0200

    Reading from file rather than from HTTPS

--- IO_methods.go
+++ IO_methods.go
@@ -11,7 +11,7 @@ import (
 var myClient = &http.Client{Timeout: 10 * time.Second}
 
 // Simple HTTPS client to parse JSON
-func getJSON(url string, target interface{}) error {
+func httpsGetJSON(url string, target interface{}) error {
 	r, err := myClient.Get(url)
 	if err != nil {
 		return err
@@ -19,6 +19,12 @@ func getJSON(url string, target interface{}) error {
 	defer r.Body.Close()
 
 	return json.NewDecoder(r.Body).Decode(target)
+}
+
+// readJSON : Loads an structure with JSON from a file
+func readJSON(filename string, target interface{}) {
+	fileData, _ := ioutil.ReadFile(filename)
+	_ = json.Unmarshal([]byte(fileData), &target)
 }
 
 // Write a structure type to a JSON file
--- goCesarCypher.go
+++ goCesarCypher.go
@@ -1,14 +1,30 @@
 package main
 
-func main() {
-	// HTTPS address of my JSON file
-	var myURL = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=b566cecbba033c449a449a8d3e16fb69e10bddcc"
+// Read from file hardcoded condition
+const readFromWeb = false
 
-	// Read problem case from the url file
+// HTTPS address of my JSON file
+const myURL = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=b566cecbba033c449a449a8d3e16fb69e10bddcc"
+
+// Input file name
+const inFileName = "data/question.json"
+
+// Output file name
+const outFileName = "data/answer.json"
+
+func main() {
+	// Problem Description
 	description := ProblemCase{}
-	getJSON(myURL, &description)
+
+	if readFromWeb {
+		// Read description from HTTPS
+		httpsGetJSON(myURL, &description)
+	} else {
+		// Read description from file
+		readJSON(inFileName, &description)
+	}
 
 	readPloblemCaseJSON(description)
 
-	writeJSON("data/answer.json", description)
+	writeJSON(outFileName, description)
 }
