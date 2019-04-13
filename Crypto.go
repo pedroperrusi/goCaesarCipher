a3d36d91f2ec844a7cd63b136c867b8d26b42f37
pedroperrusi <suruagyperrusi@etu.unistra.fr>
2019-04-13 13:14:34 +0200

    Refactored a function to unencrypt a single char

--- Crypto.go
+++ Crypto.go
@@ -7,12 +7,17 @@ func replaceAtIndex(str string, replacement rune, index int) string {
 	return str[:index] + string(replacement) + str[index+1:]
 }
 
+func unencryptChar(nCases uint8, char uint8) rune {
+	return rune(char - nCases)
+}
+
 func invCesarCypher(nCases uint8, src string) string {
 	var dst = strings.ToLower(src)
 
 	for i := 0; i < len(dst); i++ {
 		if dst[i] >= 'a' && dst[i] <= 'z' {
-			dst = replaceAtIndex(dst, rune(dst[i]-nCases), i)
+			var newChar = unencryptChar(nCases, dst[i])
+			dst = replaceAtIndex(dst, newChar, i)
 		}
 	}
 	return dst
